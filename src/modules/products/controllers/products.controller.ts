fd1222ffefd927b38dbfa61f1a45cb38462bb168
Alef Natan <alefnatan.g32@gmail.com>
2023-10-26 14:51:47 -0300

    fix: Corrige exportação em products.controller.ts

--- src/modules/products/controllers/products.controller.ts
+++ src/modules/products/controllers/products.controller.ts
@@ -1,5 +1,10 @@
+import { Request, Response } from 'express';
+
+
 class ProductsController {
     static async create(request: Request, response: Response) {
-        const createProductService = new CreateUserService(new ProductsDBRepository())
+        //const createProductService = new CreateUserService(new ProductsDBRepository())
     }
 }
+
+export default ProductsController;
