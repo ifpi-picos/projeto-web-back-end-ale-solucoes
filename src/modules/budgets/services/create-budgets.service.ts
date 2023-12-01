e81687b10cf8fab452fa174fff6ed0ac1be3273a
ezin1 <eziofeitt15@gmail.com>
2023-12-01 17:32:34 -0300

    feat: teeeest

--- src/modules/budgets/services/create-budgets.service.ts
+++ src/modules/budgets/services/create-budgets.service.ts
@@ -41,46 +41,93 @@ export class CreateBudgetService {
         font: 'Helvetica'
       },
       content: [
-        { text: 'This is a header', style: 'header' },
-        'No styling here, this is a standard paragraph',
-        { text: 'Another text', style: 'anotherStyle' },
-        { text: 'Multiple styles applied', style: ['header', 'anotherStyle'] }
+        { text: 'Orçamento', style: 'header' },
+        { text: `Nº do Orçamento: ${123}` },
+        { text: `Cliente: ${123}` },
+        {
+          text: `Data: <span class="math-inline">\{new Date\(\)\.toLocaleDateString\(\)\}\` \},
+\{ text\: \`\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\` \},
+\{ text\: \`Produto\`, style\: 'header' \},
+\{ text\: \`Quantidade\`, style\: 'header' \},
+\{ text\: \`Valor Unitário\`, style\: 'header' \},
+\{ text\: \`Valor Total\`, style\: 'header' \},
+\{ text\: \`\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\` \},
+\.\.\.budget\.products\.map\(\(product\) \=\> \(\{
+text\: product\.name,
+\}\)\),
+\.\.\.budget\.products\.map\(\(product\) \=\> \(\{
+text\: product\.quantity\.toString\(\),
+\}\)\),
+\.\.\.budget\.products\.map\(\(product\) \=\> \(\{
+text\: product\.price\.toString\(\),
+alignment\: 'right'
+\}\)\),
+\.\.\.budget\.products\.map\(\(product\) \=\> \(\{
+text\: \(product\.quantity \* product\.price\)\.toFixed\(2\),
+alignment\: 'right'
+\}\)\),
+\{ text\: \`\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\` \},
+\{ text\: \`Subtotal\: R</span> <span class="math-inline">\{budget\.products\.reduce\(\(sum, product\) \=\> sum \+ product\.price \* product\.quantity, 0\)\.toFixed\(2\)\}\`, style\: 'subheader' \},
+\{ text\: \`Desconto\: R</span> <span class="math-inline">\{budget\.discount\.toFixed\(2\)\}\`, style\: 'subheader' \},
+\{ text\: \`Total\: R</span> ${123}`, style: 'header'
+        },
+
+        { text: `---------------------------------------------------------------------------------------` },
+
+        { text: `Assinatura do Cliente`, style: 'left' },
+        { text: `Assinatura da Empresa`, style: 'right'},
+
+        { text: `Orçamento gerado por orçamento.com.br`, style: 'right' },
       ],
+      styles: {
+        header: {
+          fontSize: 14,
+          bold: true
+        },
+        subheader: {
+          fontSize: 12
+        },
+        left: {
+          alignment: 'left'       
+        },
+        right: {
+          alignment: 'right'       
+        },
+  }
+};
 
-    }
+const pdfDoc = printer.createPdfKitDocument(
+  docDefinition
+);
+const chunks: any[] = [];
 
-    const pdfDoc = printer.createPdfKitDocument(
-      docDefinition
-    );
-    const chunks: any[] = [];
+pdfDoc.on('data', (chunk) => {
+  chunks.push(chunk);
+});
 
-    pdfDoc.on('data', (chunk) => {
-      chunks.push(chunk);
-    });
-    
-    pdfDoc.end();
-    // pdfDoc.on('end', () => {
-    //   const result = Buffer.concat(chunks).toString('base64');
-    //   console.log(result)
-    // }
-    // );
-    const base64 = await new Promise((resolve, reject) => {
-      pdfDoc.on('end', () => {
-        const result = Buffer.concat(chunks).toString('base64');
-        resolve(result);
-      });
-      pdfDoc.on('error', (err) => {
-        reject(err);
-      });
-    });
+pdfDoc.end();
+pdfDoc.on('end', () => {
+  const result = Buffer.concat(chunks);
+  
+}
+);
+const base64 = await new Promise((resolve, reject) => {
+  pdfDoc.on('end', () => {
+    const result = Buffer.concat(chunks).toString('base64');
+    resolve(result);
+  });
+  pdfDoc.on('error', (err) => {
+    reject(err);
+  });
+});
 
-    // pdfDoc.pipe(fs.createWriteStream('document.pdf'));
+pdfDoc.pipe(fs.createWriteStream('document.pdf', { encoding: 'base64' }));
 
-    // const test= await generatePdf(budget);
-    // const budgetCreated = await this.budgetsRepository.create(
-    //   budget
-    // );
-    return base64;
+// const test= await generatePdf(budget);
+// const budgetCreated = await this.budgetsRepository.create(
+//   budget
+// );
+return base64;
   }
 
 }
