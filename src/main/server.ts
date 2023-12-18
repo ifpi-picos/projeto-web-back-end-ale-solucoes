6464faa35629cca2590de48acd84fc3db984139a
ezin1 <eziof.dev@gmail.com>
2023-12-18 13:36:41 -0300

    ...

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,11 +3,10 @@ import { createHttpTerminator } from 'http-terminator';
 import { logger } from '../shared/utils/logger';
 import { setupApp } from './config/app';
 
-const port = 3010
 export const app = setupApp();
-
+const port = process.env.PORT;
 export const server = app.listen(port, () => {
-  logger.info(`Server running at: http://localhost:${process.env.PORT}`);
+  logger.info(`Server running at: http://localhost:${port}`);
 });
 
 // terminator
